c7a75599831333da76aa4a1497f2b132e5bf6923
ysaotome <y@sotm.jp>
2012-09-13 10:26:42 +0900

    Update config

--- contexts/krm/param.php
+++ contexts/krm/param.php
@@ -3,4 +3,3 @@ $container->setParameter('c4sa.mysql.db', $_SERVER['C4SA_MYSQL_DB']);
 $container->setParameter('c4sa.mysql.host', $_SERVER['C4SA_MYSQL_HOST']);
 $container->setParameter('c4sa.mysql.user', $_SERVER['C4SA_MYSQL_USER']);
 $container->setParameter('c4sa.mysql.password', $_SERVER['C4SA_MYSQL_PASSWORD']);
-?>
